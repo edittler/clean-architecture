cec8762b18cdad12eb2cde45186baa990719ba23
Ezequiel Pérez Dittler <ezeperez26@users.noreply.github.com>
2016-05-25 18:27:02 -0300

    Proyecto inicial de iOS También agrego .gitignore y directorio para API.

---
+++ ios/FIUBA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FIUBA
+//
+//  Created by Ezequiel Pérez Dittler on 25/5/16.
+//  Copyright © 2016 FIUBA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
